523d12a5efda5dd3fa885334241265176b0bd311
JBrow95 <browningjustin95@gmail.com>
2018-06-11 14:23:11 -0400

    create func that takes the users input and outputs the returned values

--- calc.rb
+++ calc.rb
@@ -4,30 +4,3 @@ def get_v(n)	#function that calls var n
 		gets.chomp.to_i
 	}
 end
-
-	puts "[add], [multiply], [subtract] ?"
-	repsonse = gets.chomp
-
-case repsonse
-
-when 'add'
-  puts "Enter values"
-  operator = :+
-
- when "multiply"
- 	puts "Enter values"
- 	operator = :*
-
- when "subtract"
- 	puts "Enter values"
- 	operator = :-
- else
- 	puts "Alert"
- 	exit
- end
-
- puts "How many values would you like to use?"
-	nov = gets.to_i
-
-answer = get_v(nov).inject(operator)
-puts "Your answer is:  #{ answer }"
