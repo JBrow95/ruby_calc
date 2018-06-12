312d220885f8aef6d3a3952464c9fe488a8b7856
JBrow95 <browningjustin95@gmail.com>
2018-06-12 13:00:01 -0400

    Made variable to get users input on how many values they want to use

--- calc.rb
+++ calc.rb
@@ -1,29 +1,38 @@
-def get_v(n)	#function that calls var n
-	n.times.map.with_index { |n|	# .map outputs the returned value
-		puts "Enter value ##{ 1 + n }:"	
+
+def get_v(n)
+	n.times.map.with_index { |n| 
+		puts "Enter Value #{ 1 + n }"
 		gets.chomp.to_i
 	}
 end
 
-	puts "[add], [multiply], [subtract] ?"
-	repsonse = gets.chomp
+	puts "[add], [multiply], [subtract], [divide] ?"
+
+repsonse = gets.chomp
 
 case repsonse
-
-when 'add'
-  puts "Enter values"
+ when 'add'
+  puts "Enter 2 values you want to add"
   operator = :+
 
  when "multiply"
- 	puts "Enter values"
+ 	puts "Enter 2 values you want to multiply"
  	operator = :*
 
  when "subtract"
- 	puts "Enter values"
+ 	puts "Enter 2 values you want to subtract"
  	operator = :-
- else
- 	puts "Invalid Operator!"
+
+ when "divide"
+ 	puts "Enter 2 values you want to divide"
+ 	operator = :/ 
+
+ when "exit"
+ 	puts "Goodbye"
  	exit
- end
+end
+
+	puts "How many values would you like to use?"
+	nov = gets.to_i
 
 
